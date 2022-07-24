560493a871476d1d7c3634da6fd8cabdb81d996f
grafvadim <54978268+grafvadim@users.noreply.github.com>
2022-07-24 23:56:32 +0300

    Update 2.py
    еще код

--- 2.py
+++ 2.py
@@ -9,3 +9,19 @@ numbers_2 = numbers[1]
 sum = int(numbers_1) + int(numbers_2)
 # Вывод
 print (numbers_1, '+', numbers_2, '=', sum)
+
+# Работа с условиями
+sun = input("Введите 1 если солнечно, и 2 если посмурно ")
+if sun == '1':
+    d = "Можно загарать"
+else:
+    d = "Смотрим телевизор"
+print(d)
+
+myname = input("Введите логин: ")
+mypass = input("Введите пароль: ")
+if myname == 'Ivan' and mypass == '12345678':
+    print("Добро пожаловать админ!")
+else:
+    print("Доступ закрыт")
+    
