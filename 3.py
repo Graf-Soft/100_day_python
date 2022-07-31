84c17574226211d9e629de10c3c9dcbcb821d03c
grafvadim <54978268+grafvadim@users.noreply.github.com>
2022-07-31 21:44:41 +0300

    Create 3.py
    Новый код

---
+++ 3.py
@@ -0,0 +1,4 @@
+share_len = float(input('Ведите длину прямоугольника: '))
+share_width = float(input('Ведите ширину прямоугольника: '))
+share_area = share_len * share_width
+print(f'Площадь прямоугольника: {share_area}')
