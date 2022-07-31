d966cbaead8137cf11b4cff33c6bff753bc66ba5
grafvadim <54978268+grafvadim@users.noreply.github.com>
2022-07-31 22:49:43 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,12 @@
-share_len = float(input('Ведите длину прямоугольника: '))
-share_width = float(input('Ведите ширину прямоугольника: '))
+# Первое задание
+share_len = float(input("Ведите длину прямоугольника: "))
+share_width = float(input("Ведите ширину прямоугольника: "))
 share_area = share_len * share_width
-print(f'Площадь прямоугольника: {share_area}')
+print(f"Площадь прямоугольника: {share_area}")
+
+# Второе задание
+age = int(input("Ведите свой возраст: "))
+if age < 18:
+    print("Проходи мимо, сынок)")
+else:
+    print("Добро Пожаловать на наш Хаб")
